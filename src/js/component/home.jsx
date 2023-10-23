25aa4d2613392a1b46df848c2a1007c088ae0602
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-23 20:47:26 +0000

    added instructions

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -24,3 +24,16 @@ const Home = () => {
 };
 
 export default Home;
+
+/*
+Let's create a MP3 player that works similar to Spotify
+
+The buttons should always remain at the bottom of the viewport (use position fixed for that). You only need to implement the Play, Pause, Next and previous buttons.
+
+ Requirements:
+1. List the songs from the Sounds API using the Fetch API.
+2. When the user clicks on a song, the player it must start playing it.
+3. When the user clicks on the "next" button the player should start playing the next song from the list, if there is no next song then it should start over by playing the first song of the list, the same applies for the "previous" button.
+4. Use the react ref attribute to get the audio tag from the DOM.
+5. Make sure to have only one <audio> tag on the entire project, use ref to change its src url.
+*/
