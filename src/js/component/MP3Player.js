5745718b755ee35bc71549851ecaa5fe25350b05
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-24 20:46:31 +0000

    added a component

---
+++ src/js/component/MP3Player.js
@@ -0,0 +1,10 @@
+import React, { useState, useRef } from 'react';
+
+const MP3Player = () => {
+    const [songs, setSongs] = useState([]);
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+
+    return (<h1>hola</h1>)
+}
+
+export default MP3Player;
