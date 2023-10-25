0581e905eb2ec53f1923bea435f07f187c59eef6
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-25 15:46:54 +0000

    added useEffect

--- src/js/component/MP3Player.js
+++ src/js/component/MP3Player.js
@@ -3,11 +3,18 @@ import React, { useState, useEffect, useRef } from 'react';
 const MP3Player = () => {
     const [songs, setSongs] = useState([]); // manage the state of songs and the current song index
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
-    const audioRef = useRef(null);
+    const audioRef = useRef(null); //create a ref for the audio element
 
-    fetch("https://playground.4geeks.com/apis/fake/sound/songs")  //call API, all the songs
-        .then((res) => res.json())  //this so that we get promise
-        .then((data) => console.log(data)); //then method again for handling promise
+    useEffect(() => {
+        // Fetch songs from the API when the component mounts
+        fetch("https://playground.4geeks.com/apis/fake/sound/songs") //call API, all the songs
+            .then((res) => res.json()) //this so that we get promise
+            .then((data) => setSongs(data)); //then method again for handling promise
+    }, []); // Empty dependency array -> effect runs once after the initial render
+
+
+
+
 
     return (<h1>hola</h1>)
 }
