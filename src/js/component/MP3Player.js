edfb5a3aaf7f980fa6646bdab3b674a5cd7f6d2f
Liliana Aqueveque <130264861+liliaqvq@users.noreply.github.com>
2023-10-25 15:43:43 +0000

    added some good ol fetching stuff

--- src/js/component/MP3Player.js
+++ src/js/component/MP3Player.js
@@ -1,8 +1,13 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 const MP3Player = () => {
-    const [songs, setSongs] = useState([]);
+    const [songs, setSongs] = useState([]); // manage the state of songs and the current song index
     const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const audioRef = useRef(null);
+
+    fetch("https://playground.4geeks.com/apis/fake/sound/songs")  //call API, all the songs
+        .then((res) => res.json())  //this so that we get promise
+        .then((data) => console.log(data)); //then method again for handling promise
 
     return (<h1>hola</h1>)
 }
